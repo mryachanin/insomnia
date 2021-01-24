f95f930d3e849ee2b27331320f333fdf6839951e
Mike Yachanin <michael@yachanin.com>
2021-01-24 04:49:18 -0800

    Replace moment with dayjs
    Significantly smaller. Should load faster.

--- src/routes/sleep.mjs
+++ src/routes/sleep.mjs
@@ -1,23 +1,23 @@
-import moment from 'moment';
+import dayjs from 'dayjs';
 import db from '../db.mjs';
 
 function start() {
-    var now = new moment();
+    var now = dayjs();
     console.log(`Sleep timer started at ${now}`);
 }
 
 function stop() {
-    var now = new moment();
+    var now = dayjs();
     console.log(`Sleep timer stopped at ${now}`);
 }
 
 function interrupt() {
-    var now = new moment();
+    var now = dayjs();
     console.log(`Sleep timer interrupted at ${now}`);
 }
 
 function rate(rating) {
-    var now = new moment();
+    var now = dayjs();
     console.log(`Sleep timer rated "${rating}" at ${now}`);
 }
 
